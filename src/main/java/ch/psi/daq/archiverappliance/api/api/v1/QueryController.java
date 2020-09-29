c284302505921aa9926eea1225767058aaffda9a
Simon Ebner <simon.ebner@psi.ch>
2020-09-29 11:55:36 +0200

    update on query api

--- src/main/java/ch/psi/daq/archiverappliance/api/api/v1/QueryController.java
+++ src/main/java/ch/psi/daq/archiverappliance/api/api/v1/QueryController.java
@@ -7,6 +7,7 @@ import ch.psi.daq.archiverappliance.api.api.v1.query.Range;
 import ch.psi.daq.archiverappliance.api.api.v1.query.data.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -20,14 +21,18 @@ public class QueryController {
     private static final Logger logger = LoggerFactory.getLogger(QueryController.class);
 
     private ArchiverQueryManager archiverManager;
+    private String backendId;
 
-    public QueryController(ArchiverQueryManager archiverManager) {
+    public QueryController(ArchiverQueryManager archiverManager, @Value("${backend.id}") String backendId) {
         this.archiverManager = archiverManager;
+        this.backendId = backendId;
     }
 
 
     @RequestMapping(value = "/query", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<ChannelResult> query(@RequestBody Query request) {
+
+        // Check whether the requested channels are actually all from this backend
 
         return Flux.fromStream(request.getChannels().stream())  // Iterate through channels
                 .flatMap(channelName -> {
@@ -40,16 +45,21 @@ public class QueryController {
                                     DataPoint dataPoint = new DataPoint();
                                     dataPoint.setTimestamp(Instant.ofEpochSecond(p.getSeconds(), p.getNanoseconds()));
 
-                                    DataPointMinMaxMeanValue value = new DataPointMinMaxMeanValue();
-                                    value.setMax(p.getValue());
-                                    value.setMin(p.getValue());
-                                    value.setMean(p.getValue());
+//                                    DataPointMinMaxMeanValue value = new DataPointMinMaxMeanValue();
+//                                    value.setMax(p.getValue());
+//                                    value.setMin(p.getValue());
+//                                    value.setMean(p.getValue());
+
+                                    DataPointRawValue value = new DataPointRawValue();
+                                    value.setValue(p.getValue());
+
                                     dataPoint.setValue(value);
                                     return dataPoint;
                                 }).collectList()  // collect all results into a list
                                 .map(l -> {  // construct the channel result
                                     ChannelDescription description = new ChannelDescription();
                                     description.setName(channelName);
+                                    description.setBackend(backendId);
                                     ChannelResult result = new ChannelResult();
                                     result.setChannel(description);
                                     result.setData(l);
--- src/main/java/ch/psi/daq/archiverappliance/api/api/v1/query/data/DataPointValue.java
+++ src/main/java/ch/psi/daq/archiverappliance/api/api/v1/query/data/DataPointValue.java
@@ -1,4 +1,7 @@
 package ch.psi.daq.archiverappliance.api.api.v1.query.data;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize(using = DataPointSerializer.class)
 public abstract class DataPointValue {
 }
